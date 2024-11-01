addb1dbf1826c9ffe081d9bd3374d4e8387bb270
Your Name <youremail@example.com>
2024-11-01 11:36:52 -0500

    Exercise1.C_ lab1_simple_functions

---
+++ simple_functions.py
@@ -0,0 +1,15 @@
+def favorite_things(name, fav_movie = "movie title", fav_musician = 'name'):
+   print(f"Name: {name}")
+   print(f"Favorite Movie: {fav_movie}")
+   print(f"Favorite Musician/Band: {fav_musician}")
+favorite_things("Aron Kidane", "Inception", "The Beatles")
+
+def Why_im_here():
+   print("I joined the Data Analyst program to develp my skills working with data")
+
+def fav_Place(place, reason):
+   print(f"One of my favorite places to visit is {place} because {reason}.") 
+   
+Why_im_here()
+fav_Place("Asmara, Eritrea", "it's the place where I grew up and holds many memories")
+fav_Place("Liverpool", "I want to watch a game at Anfield")
